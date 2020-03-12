ce4371b72aa148cee6d8fa3b2b6dff5e32727d6c
Kazak Dmitry <kazak.dmitry1989@mail.ru>
2020-03-12 17:20:06 +0300

    very easy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
+	n=Math.abs(n);
+	n=String(n);//'123'
+  var k=n.length-1;
+  str1='';
+  while(k>=0){
+	str1 +=n[k];  
+	k -=1;
+  }
+  return Number(str1);
   
 }
